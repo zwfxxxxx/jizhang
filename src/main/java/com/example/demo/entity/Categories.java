5443af791fe75161f8467e85c603356a1460115e
zwf <zwfxiaxue@163.com>
2024-02-27 21:46:40 +0800

    11

---
+++ src/main/java/com/example/demo/entity/Categories.java
@@ -0,0 +1,30 @@
+package com.example.demo.entity;
+
+public class Categories {
+    private int CategoryID;
+    private String CategoryName;
+
+    public int getCategoryID() {
+        return CategoryID;
+    }
+
+//    @Override
+//    public String toString() {
+//        return "Categories{" +
+//                "CategoryID=" + CategoryID +
+//                ", CategoryName='" + CategoryName + '\'' +
+//                '}';
+//    }
+//
+//    public void setCategoryID(int categoryID) {
+//        CategoryID = categoryID;
+//    }
+//
+//    public String getCategoryName() {
+//        return CategoryName;
+//    }
+//
+//    public void setCategoryName(String categoryName) {
+//        CategoryName = categoryName;
+//    }
+}
